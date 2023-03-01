a4e003f3350444c07aa04b4873502a891c0c248e
Thịnh Hoàng <thinht@trinam.vn>
2023-03-01 16:05:38 +0700

    up

---
+++ src/UnitMgr/UnitMgr.Domain/AggregatesModel/ApplicationRoleDTOs/ApplicationRoleValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Mgr.Core.Entities;
+using Mgr.Core.Entities.Identity;
+
+namespace UnitMgr.Domain.AggregatesModel;
+public class ApplicationRoleValidator : AbstractValidator<ApplicationRole>
+{
+    public ApplicationRoleValidator()
+    {
+        RuleFor(v => v.Name)
+               .MaximumLength(256)
+               .NotEmpty();
+        RuleFor(v => v.Description)
+               .MaximumLength(500);
+    }
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<ApplicationRole>
+            .CreateWithOptions((ApplicationRole)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
+}
+
+
